df1b265893fdcbe22bf0a2b4e267c175b5e60738
suijiafneg <suijiafeng@hotmail.com>
2024-12-07 23:28:45 +0800

    feat: 搭建路由结构

---
+++ src/appRoutes/index.jsx
@@ -0,0 +1,48 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { useAuth } from '../hooks';
+import { APP_NAME } from '../constants';
+
+const ChatInterface = lazy(() => import('../components/ChatInterface'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+const Login = lazy(() => import('../pages/LoginPage'));
+
+const ProtectedRoute = ({ children }) => {
+  const { isLoggedIn, isLoading } = useAuth();
+
+  if (isLoading) return <LoadingSpinner />;
+  return isLoggedIn ? children : <Navigate to="/login" replace />;
+};
+
+const Layout = () => {
+  return (
+    <Router>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/new" replace />} />
+          <Route path="/login" element={<Login WEBUI_NAME={APP_NAME} />} />
+          <Route
+            path="/new"
+            element={
+              <ProtectedRoute>
+                <ChatInterface />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/c/:chatId"
+            element={
+              <ProtectedRoute>
+                <ChatInterface />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default Layout;
